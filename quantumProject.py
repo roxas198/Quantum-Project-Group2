630693350144fe6e6698a2eacd0f9f2820318712
Darren Schmidt <darrenschmidt27@gmail.com>
2021-05-04 00:14:18 -0500

    Created list to search and randomly selected index in list

--- quantumProject.py
+++ quantumProject.py
@@ -1,7 +1,15 @@
 from qiskit import QuantumCircuit, assemble, Aer
+import numpy as np
 
 #Number of qubits is 4
 nQubit = 4
+
+#Gives a permutation of a list from 0 to N-1.
+L = np.random.permutation(2**nQubit)
+
+#Randomly select index from 0 to N-1
+y = np.random.randint(2**nQubit)
+print(y)
 
 #Initialize the circuit for Grover's Algorithm
 groverAlgorithm = QuantumCircuit(nQubit)
